f67bb99a5f3367ba4f995a93fd52805f3fb406b0
ali-vk <aliashrafvk7@gmail.com>
2026-01-27 18:17:25 +0530

    nextjs form submission

---
+++ foodies-app-nextjs/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately we couldn't find the requested page.</p>
+    </main>
+  );
+}
